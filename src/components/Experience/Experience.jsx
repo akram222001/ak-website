6e1d2e1c0ccdbbd8cc557a0cbec8028e1d0032bd
akram222001 <akramansari222001@gmail.com>
2025-09-27 13:45:45 +0530

    alingment setup

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -25,12 +25,12 @@ const Experience = () => {
         {experiences.map((experience, index) => (
           <div
             key={experience.id}
-            className={`flex flex-col sm:flex-row items-center md:mb-16 ${
+            className={`flex flex-col sm:flex-row items-center mb-6 md:mb-16 ${
               index % 2 === 0 ? "sm:justify-end" : "sm:justify-start"
             }`}
           >
             {/* Timeline Circle */}
-            <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
+            <div className="absolute left-[22px] sm:left-1/2 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
               <img
                 src={experience.img}
                 alt={experience.company}
